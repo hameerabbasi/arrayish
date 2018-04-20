93fa568d14407ed8926814d42767d273d67564fa
Hameer Abbasi <hameerabbasi@yahoo.com>
2018-04-20 11:35:36 +0200

    Clean up for initial PyPI release.

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ import versioneer
 with open('requirements.txt') as f:
     reqs = list(f.read().strip().split('\n'))
 
+with open('README.rst') as f:
+    long_desc = f.read()
+
 setup(
     name='arrayish',
     description='A package to allow downstream inter-compatibility for Numpy-compatible array structures',
+    long_description=long_desc,
     maintainer='Hameer Abbasi',
     maintainer_email='hameerabbasi@yahoo.com',
     packages=find_packages(
@@ -17,4 +21,5 @@ setup(
     zip_safe=False,
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    license='BSD',
 )
